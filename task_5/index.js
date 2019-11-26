4c41d3ed9787ef4b4462e953aa7dbfd9ca57258d
Killekip10 <vpinchuk2012@gmail.com>
2019-11-26 11:53:55 +0300

    remove comments

--- task_5/index.js
+++ task_5/index.js
@@ -1,10 +1,5 @@
-// const { app, container } = require('./src/container');
-
-// console.log(container.resolve('test'));
-// app.start();
 const container = require('./src/container');
 
 container
   .resolve('app')
   .start();
-// app.start();
